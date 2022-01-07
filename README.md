fc0910a75ce0728288af60feab7da5b0aa189c9c
unknown <rikardoan@gmail.com>
2022-01-06 23:27:42 -0500

    pseudo

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Quiz
 
 ##
+
 This is a simple quiz. The idea is to click on the correct answer for each of the quiz questions about javascript.
 You are timed throughout the quiz and your score is based on the correct answers and the timer.
 
 For this project, I used Dom manipulation.
 
-
 ##
 
 ##
+
 The following is the link
+https://tallerdeldiablo.github.io/Javascript-timed-quiz/
 https://tallerdeldiablo.github.io/Quiz/
-##
 
+##
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,50 +3,55 @@ var messageEl = document.getElementById("game-over");
 var startEl = document.getElementById("myBtn");
 
 var lapreguntaEl = document.querySelector("welcome");
-var questDivEl = document.getElementById("questDiv");
+var questEl = document.getElementById("question");
 var secondquestDivEl = document.getElementById("#secondQues");
 
 var firstdivEl = document.getElementById("first-screen");
 
 var wrong = 1;
-
 var score = 0;
-
 var questionnumber = 0;
 
 //,"What does Javascript"]
-
 //let questions = [question1, "Inside which HTML element do we put the JavaScript?","What does Javascript"]
 
-let questions = [
+let questionsArray = [
   {
-    question1: "What is Javascript?",
+    question: " Q 1  - What is Javascript?",
     choice1: "A programming language",
     choice2: " a style sheet language ",
     choice3: "HyperText markup language",
-    answer: 1,
+    answer: "1",
   },
   {
-    question2: "Inside which HTML element do we put the JavaScript?",
-    choice1: " script ",
+    question: " Q -2 Inside which HTML element do we put the JavaScript?",
+    choice1: "script",
     choice2: "js",
     choice3: "javascript",
-    answer: 1,
+    answer: "1",
   },
 
   {
-    question3: "What does Javascript do?",
+    question: " Q - 3 What does Javascript do?",
     choice1: "Create Structure",
     choice2: "Stilize the web",
     choice3: "Increase interactivity",
-    answer: 3,
+    answer: "3 ",
+  },
+  {
+    question: " Q - 4 Commonly used data types DO NOT include:",
+    choice1: "strings",
+    choice2: "booleans",
+    choice3: "alerts",
+    answer: "3",
   },
 ];
 
+function selectedchoice(choice) {}
 //Timer
 
 function countdown() {
-  var timeLeft = 55;
+  var timeLeft = 40;
 
   var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
@@ -62,16 +67,64 @@ function countdown() {
   }, 1000);
 }
 
-// START BUTTON
 startEl.addEventListener("click", function () {
-  countdown();
   start();
+  countdown();
 });
 
-function end() {
-  timerEl.remove;
+//*-------clear html
+function start() {
+  var myobj = document.getElementById("myBtn");
 
-  document.getElementById("question").remove();
+  //delete START BUTTON
+  myobj.remove();
+
+  var ques = 0;
+  // call funtion --> render question/choices
+  renderQ(ques);
+}
+function renderQ(indexQ) {
+  document.getElementById("question").innerHTML =
+    "#" + indexQ + questionsArray[indexQ].question;
+
+  var choicesbtns = document.getElementById("choices");
+
+  choicesbtns.innerHTML = ` <button id="btnQuestion3" onclick="selectedanswer('1',${indexQ})">${questionsArray[indexQ].choice1}</button>`;
+  choicesbtns.innerHTML += ` <button id="btnQuestion3" onclick="selectedanswer('2',${indexQ})">${questionsArray[indexQ].choice2}</button>`;
+  choicesbtns.innerHTML += ` <button id="btnQuestion3" onclick="selectedanswer('3',${indexQ})">${questionsArray[indexQ].choice3}</button>`;
+}
+
+function selectedanswer(ch, indexQ) {
+  console.log(ch);
+  console.log(indexQ);
+  if (ch == questionsArray[indexQ].answer) {
+    const correct = 0;
+    console.log("correct");
+  } else {
+    const correct = 1;
+
+    console.log(" No correct");
+  }
+  indexQ++;
+  revealtext(indexQ);
+}
+function revealtext(indexQ) {
+  if (indexQ < questionsArray.length) {
+    console.log(questionsArray.length);
+    renderQ(indexQ);
+    console.log("got to next question");
+  } else {
+    end();
+    console.log("end quiz");
+  }
+}
+
+//end  First Question
+
+function end() {
+  document.getElementById("time").remove();
+
+  document.getElementById("card-contents").remove();
 
   // Creating the form
   document.getElementById("finalForm").innerHTML += `<form action="">
@@ -81,76 +134,22 @@ function end() {
     ${score}<br><br>
     <input type="submit" value="Submit"
   </form>`;
+  // clearInterval(timeInterval);
 }
+//   //inicio
 
-//FIRST QUESTION
+//   firstdivEl.addEventListener("click", function (event) {
+//     var element = event.target;
 
-function start() {
-  var myobj = document.getElementById("myBtn");
-  myobj.remove();
-
-  document.getElementById("question").innerHTML += "1" + questions[0].question1;
-
-  let btn1 =
-    (questDivEl.innerHTML += `<button id="btncorrect"> ${questions[0].choice1} </button>`);
-
-  let btn2 =
-    (questDivEl.innerHTML += `<button> ${questions[0].choice2} </button>`);
-  let btn3 =
-    (questDivEl.innerHTML += `<button> ${questions[0].choice3} </button>`);
-
-  questDivEl.addEventListener("click", function (event) {
-    var element = event.target;
-    event.stopPropagation();
-    if (element.matches("#btncorrect")) {
-      function hide(elements) {
-        elements = elements.length ? elements : [elements];
-        for (var index = 0; index < elements.length; index++) {
-          elements[index].style.display = "block";
-        }
-      }
-      hide(document.getElementById("correctOrnot"));
-      document.getElementById("correctOrnot").innerHTML += "correct1";
-
-      element.closest("div").remove();
-
-      secondQ();
-    } else {
-      wrong = 5;
-      element.closest("div").remove();
-      alert(" WRONG ");
-      secondQ();
-    }
-  });
-
-  ////end u func
-} //end  First Question
-
-function secondQ() {
-  document.getElementById("question").innerHTML = "2" + questions[1].question2;
-
-  let btn1 =
-    (firstdivEl.innerHTML += `<button id = "btncorrect"> ${questions[1].choice1} </button>`);
-  let btn2 =
-    (firstdivEl.innerHTML += `<button> ${questions[1].choice2} </button>`);
-  let btn3 =
-    (firstdivEl.innerHTML += `<button> ${questions[1].choice3} </button>`);
-
-  //inicio
-
-  firstdivEl.addEventListener("click", function (event) {
-    var element = event.target;
-
-    if (element.matches("#btncorrect")) {
-      var state = element.getAttribute("color: green");
-      alert(" Third ");
-      element.closest("div").remove();
-      end();
-    } else {
-      wrong = 5;
-      element.closest("div").remove();
-      alert(" WRONG ");
-      end();
-    }
-  });
-}
+//     if (element.matches("#btncorrect")) {
+//       var state = element.getAttribute("color: green");
+//       alert(" Third ");
+//       element.closest("div").remove();
+//     } else {
+//       wrong = 5;
+//       element.closest("div").remove();
+//       alert(" WRONG ");
+//     }
+//   });
+//   return;
+// }
